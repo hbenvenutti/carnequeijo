3baf5c4239bb6fd326cab0e900635ec4385152d8
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-03-02 01:53:22 -0300

    feat(header): criado o logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,7 @@
+import { FaHamburger } from 'react-icons/fa';
+
 import styles from './header.module.css';
+
 
 import type { ReactElement } from 'react';
 
@@ -6,7 +9,20 @@ import type { ReactElement } from 'react';
 
 function Header(): ReactElement {
   return (
-    <header className={styles.header}>Carne e Queijo</header>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <FaHamburger />
+
+        <div className={styles.logoText}>
+          <p className={styles.carne}>
+            Carne e 
+          </p>
+          <p className={styles.queijo}>
+            Queijo
+          </p>
+        </div>
+      </div>
+    </header>
   );
 }
 
