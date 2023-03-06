d87d383027ab6c2d76080f1c23ae71eff6ccac34
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-03-06 16:51:30 -0300

    feat(fonts): adicionadas as fontes

---
+++ src/fonts/stardos.ts
@@ -0,0 +1,12 @@
+import { Stardos_Stencil } from '@next/font/google';
+
+// * ---------------------------------------------------------------------- * //
+
+const stardos = Stardos_Stencil({ 
+  weight: [ '400', '700' ], 
+  subsets: [ 'latin' ] 
+});
+
+// * ---------------------------------------------------------------------- * //
+
+export { stardos };
---
+++ src/fonts/stencil-std.ts
@@ -0,0 +1,9 @@
+import localFont from '@next/font/local';
+
+// * ---------------------------------------------------------------------- * //
+
+const stencilSTD = localFont({ src: './stencil-std-bold.otf' });
+
+// * ---------------------------------------------------------------------- * //
+
+export { stencilSTD };
