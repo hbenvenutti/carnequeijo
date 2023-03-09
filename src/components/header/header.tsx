c27debb2b7c9b4164764a645435a1bc55a0da7ae
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-03-09 02:02:21 -0300

    feat(header): implementado o header
    * Logo em svg;
    * Links que estilizam ao ativar;

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,27 +1,61 @@
+'use client';
+
 import { FaHamburger } from 'react-icons/fa';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import styles from './header.module.css';
-
+import { links } from './links';
 
 import type { ReactElement } from 'react';
+
+import { Logo } from 'ASSETS/logo';
+import { stardos } from 'FONTS/stardos';
 
 // * ---------------------------------------------------------------------- * //
 
 function Header(): ReactElement {
-  return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <FaHamburger />
+  // *** --- Contexts --------------------------------------------------- *** //
+  const pathname = usePathname();
 
-        <div className={styles.logoText}>
-          <p className={styles.carne}>
-            Carne e 
-          </p>
-          <p className={styles.queijo}>
-            Queijo
-          </p>
-        </div>
-      </div>
+  // *** --- TSX -------------------------------------------------------- *** //
+  return (
+    <header className={`${styles.header} ${stardos.className}`}>
+      <Link 
+        href="/" 
+        className={`${styles.logo}`}
+      >
+        <Logo />
+      </Link>
+
+      <nav className={styles.nav}>
+        {
+          links.map(link => {
+            const className = pathname === link.path 
+              ? styles.active 
+              : styles.inactive;
+            
+            return (
+
+              <div className={styles.link} key={link.title}>
+                <FaHamburger className={className} />
+                {/* {className === styles.active && <FaHamburger />} */}
+                
+                <Link 
+                  key={link.title}
+                  href={link.path}
+                  className={className}
+                > 
+                  {link.title} 
+                </Link>
+              </div>
+            );
+          }
+          )
+        }
+      </nav>
+      
+      <a className={styles.callToAction} href="">Faça o seu pedido</a>
     </header>
   );
 }
---
+++ src/components/header/links.ts
@@ -0,0 +1,19 @@
+const links = [
+  {
+    title: 'Cardápio',
+    path: '/cardapio'
+  },
+  {
+    title: 'Localização',
+    path: '/location'
+  },
+  {
+    title: 'Nossa História',
+    path: '/historia'
+  }
+
+] as const;
+
+// * ---------------------------------------------------------------------- * //
+
+export { links };
